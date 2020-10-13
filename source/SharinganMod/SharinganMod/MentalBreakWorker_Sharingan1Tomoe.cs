1e8b063678da276a2612d39bc343addf3d2369b4
T0mikCZ <oplpet95@gmail.com>
2020-10-13 13:55:14 +0200

    you can now get sharingan through mental breakdown experimental

---
+++ source/SharinganMod/SharinganMod/MentalBreakWorker_Sharingan1Tomoe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Emit;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace SharinganMod
+{
+    public class MentalBreakWorker_Sharingan1Tomoe : MentalBreakWorker
+    {
+		HediffDef hediffSharingan1Tomoe = DefDatabase<HediffDef>.GetNamed("Sharingan1Tomoe");
+		public override bool BreakCanOccur(Pawn pawn)
+		{
+			return pawn.IsColonist && pawn.Spawned && base.BreakCanOccur(pawn);
+		}
+
+		public override bool TryStart(Pawn pawn, string reason, bool causedByMood)
+		{
+			pawn.health.AddHediff(hediffSharingan1Tomoe, null, null, null);
+			base.TrySendLetter(pawn, "LetterSharingan1Get", reason);
+			return true;
+		}
+	}
+}
---
+++ source/SharinganMod/SharinganMod/MyDefOf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace SharinganMod
+{
+    public static class MyDefOf
+    {
+        public static Hediff Sharingan1Tomoe;
+        public static Hediff Sharingan2Tomoe;
+        public static Hediff Sharingan3Tomoe;
+        static MyDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(MyDefOf));
+        }
+    }
+}
